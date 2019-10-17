77f199770060faac2195ccca7b8041e585e85425
oladotun <aboabaoladotun@gmail.com>
2019-10-17 11:02:01 +0100

    initial commit

---
+++ src/codelagosdeliveryproject/NewProductClass.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codelagosdeliveryproject;
+
+/**
+ *
+ * @author Oladotun Aboaba
+ */
+public class NewProductClass {
+  //  String b;
+ public NewProductClass(String b){
+          //  this.b= b;
+            
+        }
+ 
+        //public void formular(){
+        //int cost = 500;
+        //lagos island calculations
+        //if("one bag".equals(b)){ 
+        //double a = cost;
+        //System.out.println("A bag costs: " + a + "naira");
+        }
+        //else if("two bags".equals(b)){
+        //double b = cost * 2;
+          //      System.out.println("The cost of delivery to bourdillon is: " + b + "naira");
+//                }
+        //}
+        //}
